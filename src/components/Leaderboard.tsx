a845066eff3da8594da439ec50d58f9caaad4a42
Sujit Sable <sujitsable92@gmail.com>
2024-01-10 01:02:41 +0530

    completed Leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -5,11 +5,79 @@ const Users = [
         point: 1000,
         color: "",
         avatar: ""
-    }
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
+    {
+        name : "Ambriel",
+        point: 1000,
+        color: "",
+        avatar: ""
+    },
 ]
 
 
 export default function Leaderboard() {
+    const elements = [];
+    for (let i = 0; i < 10; i++) {
+        elements.push(<div key={i} className="flex mt-3 w-[660px]">
+        <p className="mr-2 pt-0.5 w-[60px]">1000 p</p>
+        <div className="w-[75%] flex rounded-e-full">
+            <div className="bg-blue-800 w-full animate-jiggle"></div>
+            <div className="bg-blue-800 rounded-e-full">
+                <img src="avatar.jpg" alt="avatar" width={40} className="rounded-full border"/>
+            </div>
+            <p className="ml-3 pt-0.5 text-center">Ambriel</p>
+        </div>    
+    </div>);
+    }
+
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-6">
         <div className="">
@@ -17,18 +85,8 @@ export default function Leaderboard() {
                 <img src='logo.png' alt="img" height={25} width={25}/>
                 <p className="text-2xl font-medium ml-3">BountyMeter</p>
             </div>
-            <p>Leaderboard</p>
-            <div className="flex mt-10 w-[650px]">
-                <p className="mr-2 w-[60px]">1000 p</p>
-                <div className="w-[75%] flex rounded-e-full">
-                    <div className="bg-blue-800 w-full"></div>
-                    <div className="bg-blue-800 justify-end rounded-e-full">
-                        <img src="avatar.jpg" alt="avatar" width={35} height={35} className="rounded-full border"/>
-                    </div>
-                    
-                </div>
-                <p className="ml-2">Ambriel</p>
-            </div>
+            <p className="mt-5 mb-10 -ml-6 text-4xl">Leaderboard</p>
+            {elements}         
         </div>
       </main>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        jiggle: {
+          '0%': {width: '0'},
+          '14%': {width: '90%'},
+          '35%': {width: '70%'},
+          '55%': {width: '30%'},
+          '75%': {width: '75%'},
+          '90%': {width: '20%'},
+          '100%': {width: '100%'},
+        },
+      },
+      animation: {
+        jiggle: 'jiggle 5s 1'
+      },
     },
   },
   plugins: [],
